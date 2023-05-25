f36f4982906c9ea568a5190f68529e5b202f5ff3
Antonio Maroto <antoniomarotoblasco@gmail.com>
2023-05-25 15:01:47 +0200

    Pass remplazado por Hash

--- BBDD/Inserts.sql
+++ BBDD/Inserts.sql
@@ -1,6 +1,7 @@
 USE farmacia;
 
-INSERT INTO doctor (mail, pass, name) VALUES ('antonio@gmail.com', '1234', 'Antonio'), ('maria@gmail.com', '1234', 'Maria');
+INSERT INTO doctor (mail, pass, name) VALUES ('antonio@gmail.com', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 'Antonio'), 
+('maria@gmail.com', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 'Maria');
 
 INSERT INTO medicine (name, tmax, tmin) VALUES ('Paracetamol', 10.5, 2.3), ('Imurel', 8.7, 1.5), ('Enantyum', 15.2, 3.1), ('Enandol', 20.6, 8.4), ('Amoxicilina', 18.1, 2.3), ('Prednisona', 30.6, 5.1), ('Omeprazol', 10.4, 3.3), ('Nolotil', 35.1, 1.5);
 
@@ -15,3 +16,6 @@ INSERT INTO xip (doctor_mail, id_medicine, id_patient, date) VALUES
 ('antonio@gmail.com', 7, 'laura@gmail.com', '2023-06-12'),
 ('maria@gmail.com', 2, 'alfredo@gmail.com', '2023-12-21'),
 ('maria@gmail.com', 1, 'alfredo@gmail.com', '2023-12-21');
+
+
+-- 1234 pass: 03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4
